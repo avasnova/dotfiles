3c3d159ae267be88685c657489cf9299b794a988
nova <supernova.apex@proton.me>
2025-07-20 17:45:59 +0300

    switched from -app to normal browser window for browser shortcuts. adjusted hyprconf

--- browser-app-shortcuts/appshortcuts.sh
+++ browser-app-shortcuts/appshortcuts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chromium --class=Instagram --user-data-dir="$HOME/.config/chromium-instagram" --no-first-run --app=https://www.instagram.com &
-chromium --class=WhatsApp --user-data-dir="$HOME/.config/chromium-whatsapp" --no-first-run --app=https://web.whatsapp.com
+chromium --class=Instagram --user-data-dir="$HOME/.config/chromium-instagram" --no-first-run https://www.instagram.com &
+chromium --class=WhatsApp --user-data-dir="$HOME/.config/chromium-whatsapp" --no-first-run https://web.whatsapp.com
 
