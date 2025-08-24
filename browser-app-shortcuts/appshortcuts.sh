748700ebe41901f25e9bd46d3441813b747301a9
nova <supernova.apex@proton.me>
2025-08-25 02:49:28 +0300

    some updates... auto start vertical kpop

--- browser-app-shortcuts/appshortcuts.sh
+++ browser-app-shortcuts/appshortcuts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chromium --class=Instagram --user-data-dir="$HOME/.config/chromium-instagram" --no-first-run https://www.instagram.com &
-#chromium --class=WhatsApp --user-data-dir="$HOME/.config/chromium-whatsapp" --no-first-run https://web.whatsapp.com
+chromium --class=VerticalKpop --user-data-dir="$HOME/.config/chromium-vertical-kpop" --no-first-run https://www.youtube.com/playlist?list=PLvJROZbkIUBvMldTPeGIZCV0AnMwNBRL_ 
 
